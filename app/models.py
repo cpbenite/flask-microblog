7e67aea334fc0ef0df57fbaf9500a5b4bf3a5fb6
cpbenite <cpbenite@ucsd.edu>
2019-10-22 15:49:18 -0700

    Error Handling v0.2

--- app/models.py
+++ app/models.py
@@ -40,5 +40,5 @@ def load_user(id):
 
 followers = db.Table('followers', \
 	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')), \
-	db.Column('followed_id', db.Integer, gb.ForeignKey('user.id')) 	\
+	db.Column('followed_id', db.Integer, db.ForeignKey('user.id')) 	\
 )
