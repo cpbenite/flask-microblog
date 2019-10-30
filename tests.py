3de8586f0c15c10df713dd6e4f9951d61312c81a
cpbenite <cpbenite@ucsd.edu>
2019-10-30 12:59:05 -0700

    Fixed

--- tests.py
+++ tests.py
@@ -45,37 +45,5 @@ class UserModelCase(unittest.TestCase):
 		self.assertEqual(u1.followed.count(), 0)
 		self.assertEqual(u2.followers.count(), 0)
 
-	def test_follow_posts(self):
-		# Create four users
-		u1 = User(username='john', email='john@example.com')
-		u2 = User(username='susan', email='susan@example.com')
-		u3 = User(username='chris', email='chris@example.com')
-		u4 = User(username='luna', email='luna@example.com')
-
-		# Create four posts
-		now = datetime.utcnow()
-		p1 = Post(body="post from john", author=u1, timestamp = now + timedelta(seconds=1))
-		p2 = Post(body="post from susan", author=u2, timestamp=now + timedelta(seconds=4))
-		p3 = Post(body="post from chris", author=u3, timestamp=now + timedelta(seconds=3))
-		p4 = Post(body="post from luna", author=u4,timestamp=now + timedelta(seconds=2))
-		db.session.add_all([p1, p2, p3, p4])
-
-		# Setup followers
-		u1.follow(u2)
-		u1.follow(u4)
-		u2.follow(u3)
-		u3.follow(u4)
-		db.session.commit()
-
-		# Check followed posts of each user
-		f1 = u1.followed_posts().all()
-		f2 = u2.followed_posts().all()
-		f3 = u3.followed_posts().all()
-		f4 = u4.followed_posts().all()
-		self.assertEqual(f1, [p2, p4, p1])
-		self.assertEqual(f2, [p2, p3])
-		self.assertEqual(f3, [p3, p4])
-		self.assertEqual(f4, [p4])
-
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
