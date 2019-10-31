893952554173fd3f4cd626f92172352f6ef95875
cpbenite <cpbenite@ucsd.edu>
2019-10-31 12:58:02 -0700

    Pagination v0.2 Limited Posts_per_Page to 3

--- app/routes.py
+++ app/routes.py
@@ -17,8 +17,10 @@ def index():
 		db.session.commit()
 		flash('Your post is now live!')
 		return redirect(url_for('index'))
-	posts = current_user.followed_posts()
-	return render_template('index.html', title='Home', posts=posts, form=form)
+	page = request.args.get('page', 1, type=int)
+	posts = current_user.followed_posts().paginate(\
+		page, app.config['POSTS_PER_PAGE'], False)
+	return render_template('index.html', title='Home', posts=posts.items, form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -117,3 +119,10 @@ def unfollow(username):
 	flash('You are not following {}.'.format(username))
 	return redirect(url_for('user', username=username))
 
+@app.route('/explore')
+@login_required
+def explore():
+	page = request.args.get('page', 1, type=int)
+	posts = Post.query.order_by(Post.timestamp.desc()).paginate(\
+			page, app.config['POSTS_PER_PAGE'], False)
+	return render_template('index.html', title='Explore', posts=posts.items)
